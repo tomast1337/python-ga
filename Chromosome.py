cc4e285831c90480ba2093c03bf1abe2c861c833
tomast1337 <vycasnicolas@gmail.com>
2023-05-04 00:22:08 -0300

    Added linear_scaling and more fields to the history

--- Chromosome.py
+++ Chromosome.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 Bit = Union["0", "1"]
 
+
 class PopulationParams(TypedDict):
     mutation_rate: float
     crossover_rate: float
@@ -14,11 +15,12 @@ class PopulationParams(TypedDict):
     elitism_number: int
     steady_state: bool
     windowing: bool
-    duplicate_elitism: bool
+    linear_scaling: bool
+    duplicate_selection: bool
 
 
 GENE_SIZE = 22
-CHROMOSOME_SIZE = 44
+CHROMOSOME_SIZE = GENE_SIZE * 2
 
 
 def f6(x: float, y: float) -> float:
@@ -35,8 +37,7 @@ class Chromosome:
     def __init__(self, genes: List[Bit], size: int = CHROMOSOME_SIZE):
         super().__init__()
         if len(genes) != size:
-            raise ValueError(
-                f"Chromosome must have at least {size} gene. {len(genes)}")
+            raise ValueError(f"Chromosome must have at least {size} gene. {len(genes)}")
         if not all([gene in ["0", "1"] for gene in genes]):
             raise ValueError("Chromosome genes must be represented by 0 or 1.")
         self.genes = genes
@@ -132,6 +133,7 @@ class Population:
     steady_state: bool
     duplicate_selection: bool
     windowing: bool
+    linear_scaling: bool
     size: int
     history: pd.DataFrame
 
@@ -143,8 +145,20 @@ class Population:
         elitism_number: int = 1,
         steady_state: bool = False,
         duplicate_selection: bool = False,
+        linear_scaling: bool = False,
         windowing: bool = False,
     ):
+        if steady_state:
+            if linear_scaling and windowing:
+                raise ValueError(
+                    "Linear scaling and windowing cannot be used together."
+                )
+        else:
+            if duplicate_selection or linear_scaling or windowing:
+                raise ValueError(
+                    "Duplicate selection, linear scaling and windowing cannot be with out steady state."
+                )
+
         # history of best individuals
         self.history = pd.DataFrame(
             columns=[
@@ -163,6 +177,7 @@ class Population:
         self.steady_state = steady_state  # use steady state instead of generational
         self.duplicate_selection = duplicate_selection  # allow duplicate elitism
         self.windowing = windowing  # use windowing instead of truncation
+        self.linear_scaling = linear_scaling  # use linear scaling
         self.size = size  # population size
 
         # generate random individuals
@@ -178,8 +193,6 @@ class Population:
         sorted_pop = sorted(self.individuals, key=lambda x: x.fitness, reverse=True)[
             :count
         ]
-
-        # print([x.fitness for x in sorted_pop])
         # clone the best ones
         return [x.copy() for x in sorted_pop[:count]]
 
@@ -196,6 +209,9 @@ class Population:
             "best_x": self.get_best_n(1)[0].to_coords()[0],
             "best_y": self.get_best_n(1)[0].to_coords()[1],
             "best_chromosome": self.get_best_n(1)[0].__repr__(),
+            "mean_fitness": np.mean([x.fitness for x in self.individuals]),
+            "std_fitness": np.std([x.fitness for x in self.individuals]),
+            "min_fitness": np.min([x.fitness for x in self.individuals]),
         }
         self.history = pd.concat(
             [self.history, pd.DataFrame(data, index=[0])], ignore_index=True
@@ -204,31 +220,41 @@ class Population:
     def _steady_state_selection(self):
         # based on the fitness of each individual, select the best ones
         # step 1: sort the population by fitness
-        sorted_pop = sorted(
-            self.individuals, key=lambda x: x.fitness, reverse=True)
+        sorted_pop = sorted(self.individuals, key=lambda x: x.fitness, reverse=True)
         # step 2: do a non uniform random selection of the individuals, the best ones have a higher chance of being selected
         weights = [i.fitness for i in sorted_pop]
-        
         # if windowing is enabled, normalize the weights push the higher weights to the top and lower weights to the bottom
         if self.windowing:
             max_weight = max(weights)
             min_weight = min(weights)
-            weights = [(weight - min_weight) / (max_weight - min_weight) for weight in weights]
+            new_weights = [
+                (weight - min_weight) / (max_weight - min_weight) for weight in weights
+            ]
+            weights = new_weights
+
+        # linear_scaling
+        if self.linear_scaling:
+            weights = list(reversed(range(1, len(weights) + 1)))
 
         # normalize the weights
-        weights = [i/sum(weights) for i in weights]
+        weights = [i / sum(weights) for i in weights]
 
         # step 3: select the individuals
         # p is the probability of each individual to be selected
-        selected = np.random.choice(sorted_pop, size=self.size//4, p=weights, replace=self.duplicate_selection)
-        
+        selected = np.random.choice(
+            sorted_pop,
+            size=self.size // 10,
+            p=weights,
+            replace=self.duplicate_selection,
+        )
+
         # step 4: form the selected individuals, generate the new population
         self.individuals = []
-        for _ in range(4):
-            self.individuals.extend(selected)
+        for _ in range(10):
+            copy = [i.copy() for i in selected]
+            self.individuals.extend(copy)
+        # step 5: crossover the new population
         self.crossover_population()
-        
-        # step 5: mutate the new population
         self.mutate_population()
 
     def _non_steady_state_selection(self):
@@ -241,8 +267,7 @@ class Population:
             self._steady_state_selection()
         else:
             self._non_steady_state_selection()
-        self.individuals = the_elite + \
-            self.individuals[self.elitism_number:]
+        self.individuals = the_elite + self.individuals[self.elitism_number :]
 
     def mutate_population(self):
         for individual in self.individuals:
@@ -287,20 +312,15 @@ class ExperimentSet:
         print("=" * 25)
         print("POPULATION PARAMS")
         print("=" * 25)
-        print("Mutation rate:         ",
-              self.populations_param["mutation_rate"])
-        print("Crossover rate:        ",
-              self.populations_param["crossover_rate"])
-        print("Population size:       ", self.populations_param["size"])
-        print("Elitism number:        ",
-              self.populations_param["elitism_number"])
-        print("Steady state:          ",
-              self.populations_param["steady_state"])
-        print("Duplicate elitism:     ",
-              self.populations_param["duplicate_elitism"])
-        print("Fitness function:      ", self.fitness_func.__name__)
-        print("Number of generations: ", self.n_generations)
-        print("Number of experiments: ", self.n_experiments)
+        print("Mutation rate:\t", self.populations_param["mutation_rate"])
+        print("Crossover rate:\t", self.populations_param["crossover_rate"])
+        print("Population size:\t", self.populations_param["size"])
+        print("Elitism number:\t", self.populations_param["elitism_number"])
+        print("Steady state:\t", self.populations_param["steady_state"])
+        print("Duplicate selection:\t", self.populations_param["duplicate_selection"])
+        print("Fitness function:\t", self.fitness_func.__name__)
+        print("Number of generations\t", self.n_generations)
+        print("Number of experiments\t", self.n_experiments)
         print("=" * 25)
 
         for i in range(self.n_experiments):
@@ -311,60 +331,116 @@ class ExperimentSet:
             self.populations[i].train(self.n_generations)
 
     def get_mean_history(self):
-        df = pd.DataFrame(columns=["Generation", "Mean Fitness"])
+        df = pd.DataFrame(
+            columns=["Generation", "Mean Fitness", "9s after the decimal", "The best"]
+        )
 
         # Get best_fitness from each population in each generation
-        historys = [population.history for population in self.populations]
+        history = [population.history for population in self.populations]
         for gen in range(self.n_generations):
+            # best_fitnesses in generation between all populations
+            best_fitnesses = np.max(
+                [history[i].iloc[gen]["best_fitness"] for i in range(len(history))]
+            )
+
+            decimal = f"{best_fitnesses:.10f}".split(".")[1]
+            count = 0
+            for char in decimal:
+                if char == "9":
+                    count += 1
+                else:
+                    break
+
             data = {
                 "Generation": gen,
                 "Mean Fitness": np.mean(
-                    [
-                        historys[i].iloc[gen]["best_fitness"]
-                        for i in range(len(historys))
-                    ]
+                    [history[i].iloc[gen]["best_fitness"] for i in range(len(history))]
+                ),
+                "9s after the decimal": count,
+                "The best": best_fitnesses,
+                "Mean of Means": np.mean(
+                    [history[i].iloc[gen]["mean_fitness"] for i in range(len(history))]
+                ),
+                "Min fitness": np.min(
+                    [history[i].iloc[gen]["min_fitness"] for i in range(len(history))]
                 ),
             }
-            df = pd.concat([df, pd.DataFrame(data, index=[0])],
-                           ignore_index=True)
+            df = pd.concat([df, pd.DataFrame(data, index=[0])], ignore_index=True)
         return df
 
 
-def run_experiments():
-    populations_param = {
-        "mutation_rate": 0.1,
-        "crossover_rate": 0.8,
-        "size": 100,
-        "elitism_number": 1,
-        "steady_state": True,
-        "duplicate_elitism": False,
-    }
-    ExperimentSet(
-        populations_param=populations_param,
-        fitness_func=f6,
-    ).run()
+def test():
+    print("Hello World")
+    # Test mutation
+    a = Chromosome.from_bit_str("00000000000000000000000000000000000000000000")
+    b = Chromosome.from_bit_str("11111111111111111111111111111111111111111111")
+    print("a:", str(a))
+    a.mutate()
+    print("a:", str(a), "mutated")
+    print("b:", str(b))
+    b.mutate()
+    print("b:", str(b), "mutated")
 
-def main():
-    es = ExperimentSet(
-        populations_param={
-            "mutation_rate": 0.1,
-            "crossover_rate": 0.8,
-            "size": 100,
-            "elitism_number": 1,
-            "steady_state": True,
-            "duplicate_elitism": False,
-        },
-        n_experiments=20,
-        n_generations=40,
-        fitness_func=f6,
-        windowing=False,
+    # Test crossover
+    c = Chromosome.from_bit_str("01010101010101010101010101010101010101010101")
+    d = Chromosome.from_bit_str("11111111111111111111111111111111111111111111")
+    print("c:", str(c))
+    print("d:", str(d))
+    c.crossover(d)
+    print("c:", str(c), "crossovered")
+    print("d:", str(d), "crossovered")
+
+    # Test to_coords
+    print("a coords:", a.to_coords())
+    print("b coords:", b.to_coords())
+    print("c coords:", c.to_coords())
+    print("d coords:", d.to_coords())
+
+    # Test fitness
+    print("a fitness:", a.fitness)
+    print("b fitness:", b.fitness)
+    print("c fitness:", c.fitness)
+    print("d fitness:", d.fitness)
+
+
+def test_pop():
+    # population
+    population = Population(
+        size=100,
+        elitism_number=10,
+    )
+    population.train(40)
+    population_history = population.history
+    print(population.history)
+
+    # plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    ax.scatter(
+        population_history["best_x"],
+        population_history["best_y"],
+        population_history["best_fitness"],
+        c="r",
+        marker="x",
     )
 
-    es.run()
-    x = es.get_mean_history()
-    plt.plot(x["Generation"], x["Mean Fitness"])
+    # add f6(x,y) surface
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = f6(X, Y)
+    ax.plot_surface(X, Y, Z, alpha=0.3)
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("f6(x,y)")
+
+    plt.show()
+
+    # fitness history
+    plt.plot(population_history["generation"], population_history["best_fitness"])
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    test()
